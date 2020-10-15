7c77ed7efdd7571763af9a62c3951bee7e5a2b37
codie <constantin.diez@gmail.com>
2020-10-15 14:27:28 +0200

    1.3.1post1 version tag

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ import lasso
 import numpy as np
 from setuptools import setup, find_packages
 
+
 def collect_tests():
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('.', pattern='test_*.py')
     return test_suite
 
+
 def main():
 
     setup(name='lasso-python',
-          version='1.3.1',
+          version='1.3.1post1',
           description='A next-generation CAE Python Library.',
           author='Lasso GmbH',
           author_email='lasso@lasso.de',
@@ -33,25 +35,24 @@ def main():
           ],
           packages=find_packages(),
           package_data={
-              '': ['*.png', '*.html', '*.js', '*.so', '*.dll', '*.txt','*.css'],
+              '': ['*.png', '*.html', '*.js', '*.so', '*.dll', '*.txt', '*.css'],
           },
           test_suite='setup.collect_tests',
           zip_safe=False,
           classifiers=[
-            'Development Status :: 4 - Beta',
-            'Topic :: Scientific/Engineering',
-            'Intended Audience :: Science/Research',
-            'Topic :: Utilities',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8']
+              'Development Status :: 4 - Beta',
+              'Topic :: Scientific/Engineering',
+              'Intended Audience :: Science/Research',
+              'Topic :: Utilities',
+              'Operating System :: Microsoft :: Windows',
+              'Operating System :: POSIX :: Linux',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8']
           )
 
 
 if __name__ == "__main__":
     main()
-    
